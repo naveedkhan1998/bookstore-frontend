9875f6a4fdbbf309ba7fcfbd57e581ba98cca02f
nkhan364 <naveedkhan13041998@gmail.com>
2023-11-12 10:05:31 -0500

    basic navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+// App.js
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./layouts/Navbar";
 import Footer from "./layouts/Footer";
 import FetchTest from "./components/FetchTest";
@@ -6,12 +8,20 @@ import FetchTest from "./components/FetchTest";
 function App() {
   return (
     <>
-      <div className="flexCenter  flex flex-col">
-        <Navbar />
-        <FetchTest />
-        <h1 className="text-small paddings">Hello</h1>
-        <Footer />
-      </div>
+      <Router>
+        <div className="flex flex-col min-h-screen">
+          <Navbar />
+          <div className="flex-1 flex flex-col items-center justify-center">
+            <Routes>
+              <Route path="/">Home</Route>
+              <Route path="/about">About</Route>
+            </Routes>
+            <FetchTest />
+            <h1 className="text-small paddings">Hello</h1>
+          </div>
+          <Footer />
+        </div>
+      </Router>
     </>
   );
 }
