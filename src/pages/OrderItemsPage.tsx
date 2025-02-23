96523a083920b4b305bd7fbb12a4d6ef95910da8
nkhan364 <naveedkhan13041998@gmail.com>
2025-02-23 13:37:22 -0500

    order histry

--- src/pages/OrderItemsPage.tsx
+++ src/pages/OrderItemsPage.tsx
@@ -10,17 +10,20 @@ const OrderItemsPage = () => {
 
   return (
     <Modal>
-      <div className="grid grid-cols-[auto,fr] w-full h-full items-center p-6">
-        <h1 className="p-6 mb-2 text-2xl font-bold ">Order Items</h1>
+      <div className="grid grid-cols-[auto,1fr] w-full h-full items-center p-6">
+        <h1 className="p-6 mb-2 text-2xl font-bold">Order Items</h1>
         <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(200px,1fr))]">
           {Object.keys(entry.items).map((itemKey) => (
-            <div className="flex flex-col items-center justify-center">
-              <BookComponent key={itemKey} book_id={itemKey} />
+            <div
+              key={itemKey}
+              className="flex flex-col items-center justify-center"
+            >
+              <BookComponent book_id={itemKey} />
               <p className="pt-6">Quantity: {entry.items[itemKey]}</p>
             </div>
           ))}
         </div>
-        <h1 className="p-6 mt-10 text-2xl font-bold border rounded-md ">
+        <h1 className="p-6 mt-10 text-2xl font-bold border rounded-md">
           Total Price: {entry.price.toFixed(2)} $
         </h1>
       </div>
